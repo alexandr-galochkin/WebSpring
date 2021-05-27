0550bbc51aab20c8a7e9f11f372ba3eefd0a4f2f
alexandr-galochkin <43029653+alexandr-galochkin@users.noreply.github.com>
2021-05-27 10:46:48 +0300

    Initial commit

---
+++ src/main/java/ru/webspring/dao/PersonDAO.java
@@ -0,0 +1,41 @@
+package ru.webspring.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.webspring.models.Person;
+
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?)", person.getName());
+    }
+
+    public void update(int id, Person updatedPerson) {
+        jdbcTemplate.update("UPDATE Person SET name=? WHERE id=?", updatedPerson.getName(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+    }
+}
---
+++ src/main/java/ru/webspring/models/Person.java
@@ -0,0 +1,37 @@
+package ru.webspring.models;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 20, message = "Length should be between 2 and 20 symbols")
+    private String name;
+
+    public Person() {
+
+    }
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
